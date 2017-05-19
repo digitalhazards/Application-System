e7683598925ea6794f94cb20352206f9e887e3b0
James Slattery <james@slattery.tech>
2017-05-19 15:40:38 -0700

    materialize

---
+++ public/assets/nav.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".dropdown-button").dropdown();
+  $('.button-collapse').sideNav();
+  (function($){
+    $(function(){
+
+      $('.button-collapse').sideNav();
+
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+});
